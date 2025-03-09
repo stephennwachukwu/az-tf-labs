ca4207f0620725754303ddc916b0e2f3b4470a00
stephen_nwachukwu <stephennwac007@gmail.com>
2025-03-09 21:30:37 +0100

    Reusable Infra with modules in Azure

---
+++ lab4/main.tf
@@ -0,0 +1,32 @@
+# Terraform
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.40.0"
+    }
+  }
+}
+ 
+#Azure provider
+provider "azurerm" {
+  features {}
+}
+ 
+#Create Storage Account
+module "storage_account" {
+  source    = "./modules/storage-account"
+ 
+  saname    = "sacal13391141"
+  rgname    = "cal-1339-114"
+  location  = "westus"
+}
+ 
+#Create Storage Account
+module "storage_account2" {
+  source    = "./modules/storage-account"
+ 
+  saname    = "sacal13391142"
+  rgname    = "cal-1339-114"
+  location  = "westus"
+}
---
+++ lab4/modules/storage-account/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.40.0"
+    }
+  }
+}
+ 
+resource "azurerm_storage_account" "sa" {
+  name                     = var.saname
+  resource_group_name      = var.rgname
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+ 
+}
